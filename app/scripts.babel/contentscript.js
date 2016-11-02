2e01829d6a72d1cd56bf4c31ba0e85602c30471b
franzherzog <kontakt@franzherzog.de>
2016-11-02 20:59:13 +0100

    render moved into checkbox oop

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,9 +1,13 @@
 'use strict';
 //import Checkbox from './switch'
 import { Checkbox } from './switch'
-// let checkbox = new Checkbox()
-window.onload = function () {
-    let checkbox = new Checkbox()
+window.onload = function createToggleSwitch() {
+    var startpage = document.getElementById('feed-main-what_to_watch')
+    var sidebar = document.getElementsByClassName('watch-sidebar-section')[1]
+    var body = document.getElementsByTagName('body')[0]
+    var timeout = null;
+    let el = startpage || sidebar
+    let checkbox = new Checkbox(el)
     checkbox.registerHandler()
     chrome.runtime.onMessage.addListener(
         function (request, sender, sendResponse) {
@@ -11,45 +15,20 @@ window.onload = function () {
                 checkbox.set(request.changedMode)
             }
         })
-}
-var body = document.getElementsByTagName('body')[0]
-function listener() {
-    var startpage = document.getElementById('feed-main-what_to_watch')
-    var sidebar = document.getElementsByClassName('watch-sidebar-section')[1]
-    var el = createInsightEl();
-    if (sidebar && sidebar.style && sidebar.style.visibility !== 'hidden') {
-        sidebar.style.visibility = 'hidden'
-        sidebar.parentElement.insertBefore(createInsightEl(), sidebar.parentElement.firstChild);
+    document.addEventListener('DOMSubtreeModified', () => {
+        if (timeout) {
+            clearTimeout(timeout)
+        } else {
+            // listener()
+        }
+        timeout = setTimeout(listener, 500)
+    }, false);
+    function listener() {
+        if (document.getElementById('distraction-toggle')) {
+            checkbox.refreshState()
+        }
+        else {
+            createToggleSwitch()
+        }
     }
-    if (startpage && startpage.style && startpage.style.visibility !== 'hidden') {
-        startpage.style.visibility = 'hidden'
-
-        startpage.parentElement.insertBefore(el, startpage.parentElement.firstChild);
-        // el.addEventListener('click', function () {
-        //     console.log('content clicked')
-        //     chrome.runtime.sendMessage({ distraction: true }, function (response) {
-        //     });
-        // })
-    }
-}
-var timeout = null;
-document.addEventListener('DOMSubtreeModified', function () {
-    if (timeout) {
-        clearTimeout(timeout)
-    } else {
-        listener()
-    }
-    timeout = setTimeout(listener, 500);
-}, false);
-
-
-function createInsightEl() {
-    var el = document.createElement('div')
-    el.innerHTML = `
-	<label class="distraction-switch">
-	<input id="distraction-toggle" type="checkbox">
-	<div class="slider round"></div>
-	</label>
-    `
-    return el
 }
--- app/scripts.babel/switch.js
+++ app/scripts.babel/switch.js
@@ -1,21 +1,54 @@
 export class Checkbox {
-    constructor() {
+    constructor(el) {
+        this.render(el)
+    }
+    createEl() {
+        var el = document.createElement('div')
+        el.innerHTML = `
+            <label class="distraction-switch">
+            <input id="distraction-toggle" type="checkbox">
+            <div class="slider round"></div>
+            </label>
+            `
+        return el
+    }
+    render(parent) {
+        if (parent && parent.style && parent.style.visibility !== 'hidden') {
+            parent.style.visibility = 'hidden'
+            parent.parentElement.insertBefore(this.createEl(), parent.parentElement.firstChild);
+        }
     }
     findDomEl() {
         return this.checkbox = document.getElementById('distraction-toggle')
     }
     setMode(state) {
-        this.checkbox.checked = state
+        if (!this.checkbox) {
+            this.findDomEl()
+        }
+        if (state === 'on') {
+            this.checkbox.checked = true
+        }
+        else {
+            this.checkbox.checked = false
+        }
+    }
+    refreshState() {
+        chrome.runtime.sendMessage({ request: 'mode' }, (response) => {
+            this.setMode(response)
+        })
     }
     registerHandler() {
         if (this.findDomEl()) {
             // get initial status of checkbox
-            chrome.runtime.sendMessage({ request: 'mode' }, (response) => {
-                this.setMode(response)
-            })
+            this.refreshState()
             this.checkbox.addEventListener('change', (ev) => {
-                let isChecked = ev.target.checked
-                chrome.runtime.sendMessage({ setMode: isChecked })
+                let mode
+                if (ev.target.checked) {
+                    mode = 'on'
+                } else {
+                    mode = 'off'
+                }
+                chrome.runtime.sendMessage({ setMode: mode })
             })
             return this.checkbox
         }
